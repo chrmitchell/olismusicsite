f305dd7c82fb64f88340b2c3f566c897f7d8f6c5
chris mitchell <spacehelmetboy@gmail.com>
2022-01-08 00:56:20 -0500

    set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import logo from "./images/brightsome-fall-25-FINAL-2k-jpg.jpg";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 
 const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="/spotify" element={<SpotifyLandingPage />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>
+              There's nothing here! <Link to="/">Go Home</Link>
+            </p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+);
+
+const Home = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="albumCoverLarge" alt="logo" />
     </header>
+    <Outlet />
   </div>
 );
+
+const SpotifyLandingPage = () => <p>spotify stuff goes here</p>;
 
 export default App;
